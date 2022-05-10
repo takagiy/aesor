413c45b3d66e667ec9461ebe6911ddd8a4de2263
takagiy <takagiy.4dev@gmail.com>
2022-05-10 12:26:38 +0900

    Add reflections and colors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Add, Div, Mul, Sub, Neg};
 
 use image::{ImageBuffer, ImageFormat, Pixel, Rgba, RgbaImage};
 
@@ -48,6 +48,13 @@ impl Vec3 {
     }
 }
 
+impl Neg for &Vec3 {
+    type Output = Vec3;
+    fn neg(self) -> Self::Output {
+        -1. * self
+    }
+}
+
 impl Add for &Vec3 {
     type Output = Vec3;
     fn add(self, rhs: Self) -> Self::Output {
@@ -93,17 +100,45 @@ impl Div<f64> for &Vec3 {
 }
 
 trait Reflect {
-    fn reflect(&self, p: &Vec3, incident: &Vec3) -> Option<(f64, f64)>;
+    fn normal_vec(&self, p: &Vec3) -> Option<(Vec3, f64)>;
+    fn reflect(&self, p: &Vec3, incident: &Vec3, sight: &Vec3) -> Option<(f64, f64, f64)> {
+        self.normal_vec(p).map(|(n, alpha)| {
+            let reflection = (incident - &(2. * incident.dot(&n) * &n)).normal();
+            (-incident.dot(&n), reflection.dot(&-sight).max(0.), alpha)
+        })
+    }
+}
 
+trait Draw {
     fn draw(&self, incident: &Vec3, img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>) {
+        self.draw_color(incident, img, &Rgba([255, 255, 255, 255]));
+    }
+    fn draw_color(&self, incident: &Vec3, img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, color: &Rgba<u8>);
+}
+
+impl<T: Reflect> Draw for T {
+    fn draw_color(&self, incident: &Vec3, img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, color: &Rgba<u8>) {
         for (x, y, px) in img.enumerate_pixels_mut() {
-            if let Some((br, alpha)) = self.reflect(&Vec3::new(x as f64, y as f64, 0.), incident) {
-                let br = (50. * br) as u8 + 200;
-                px.blend(&Rgba([br, br, br, (255. * alpha) as u8]));
+            if let Some((df, sp, alpha)) = self.reflect(&Vec3::new(x as f64, y as f64, 0.), incident, &Vec3::new(x as f64 / 10., y as f64 / 10., -1.).normal()) {
+                let df = 0.3 * df + 0.8;
+                let r = (color[0] as f64 * df + 255. * sp.powi(2)).min(255.) as u8;
+                let g = (color[1] as f64 * df + 255. * sp.powi(2)).min(255.) as u8;
+                let b = (color[2] as f64 * df + 255. * sp.powi(2)).min(255.) as u8;
+                let a = (color[3] as f64 * alpha).min(255.) as u8;
+                px.blend(&Rgba([r, g, b, a]));
             }
         }
     }
 }
+
+impl<E: Draw> Draw for Vec<E> {
+    fn draw_color(&self, incident: &Vec3, img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, color: &Rgba<u8>) {
+        for e in self {
+            e.draw_color(incident, img, color);
+        }
+    }
+}
+
 
 struct Corn {
     center: Vec3,
@@ -112,11 +147,11 @@ struct Corn {
 }
 
 impl Reflect for Corn {
-    fn reflect(&self, p: &Vec3, incident: &Vec3) -> Option<(f64, f64)> {
+    fn normal_vec(&self, p: &Vec3) -> Option<(Vec3, f64)> {
         let p = p - &self.center;
         if p.norm() <= self.radius {
             let n = Vec3::new(self.height, 0., self.radius).rot_xy(&p).normal();
-            Some((-incident.dot(&n), (self.radius - p.norm()).min(1.)))
+            Some((n, (self.radius - p.norm()).min(1.)))
         } else {
             None
         }
@@ -130,7 +165,7 @@ struct Concave {
 }
 
 impl Reflect for Concave {
-    fn reflect(&self, p: &Vec3, incident: &Vec3) -> Option<(f64, f64)> {
+    fn normal_vec(&self, p: &Vec3) -> Option<(Vec3, f64)> {
         let p = p - &self.center;
         if p.norm() <= self.radius {
             let y0 = (self.radius.powi(2) - self.depth.powi(2)) / (2. * self.depth.abs());
@@ -138,7 +173,7 @@ impl Reflect for Concave {
             let theta = (p.norm() / r).asin();
             let p_sign = -1. * self.depth / self.depth.abs();
             let n = Vec3::new(p_sign * p.x, p_sign * p.y, r * theta.cos()).normal();
-            Some((-incident.dot(&n), (self.radius - p.norm()).min(1.)))
+            Some((n, (self.radius - p.norm()).min(1.)))
         } else {
             None
         }
@@ -153,7 +188,7 @@ struct RoundBox {
 }
 
 impl Reflect for RoundBox {
-    fn reflect(&self, p: &Vec3, incident: &Vec3) -> Option<(f64, f64)> {
+    fn normal_vec(&self, p: &Vec3) -> Option<(Vec3, f64)> {
         let (rim_x, rim_y) = if p.x <= self.top_left.x {
             if p.y <= self.top_left.y {
                 (self.top_left.x, self.top_left.y)
@@ -184,7 +219,7 @@ impl Reflect for RoundBox {
             radius: self.border_radius,
             depth: self.depth,
         };
-        rim.reflect(p, incident)
+        rim.normal_vec(p)
     }
 }
 
@@ -199,16 +234,22 @@ fn main() {
         radius: 130.,
         depth: 30.,
     };
+    let tip_rim = Corn {
+        center: point(215., 85.),
+        radius: 20.,
+        height: -30.
+    };
     let tip = Concave {
         center: point(215., 85.),
         radius: 15.,
-        depth: 15.,
+        depth: 2.,
     };
-    let incident = Vec3::new(0., 1., -1.).normal();
+    let incident = Vec3::new(-0.5, 1., -1.).normal();
     let mut img = RgbaImage::new(300, 300);
     rim.draw(&incident, &mut img);
     top.draw(&incident, &mut img);
-    tip.draw(&incident, &mut img);
+    tip_rim.draw(&incident, &mut img);
+    tip.draw_color(&incident, &mut img, &Rgba([50,50,50,255]));
     img.save_with_format("out.png", ImageFormat::Png).unwrap();
     let rim = RoundBox {
         top_left: point(50., 100.),
@@ -234,7 +275,7 @@ fn main() {
         border_radius: 15.,
         depth: 5.
     };
-    let tips: Vec<_> = (0..=0)
+    let tips: Vec<_> = (-1..=1)
         .into_iter()
         .map(|i| RoundBox {
             top_left: point(100. + 12. * i as f64, 90.),
@@ -246,10 +287,9 @@ fn main() {
     let mut img = RgbaImage::new(300, 200);
     rim.draw(&incident, &mut img);
     trace.draw(&incident, &mut img);
-    slider.draw(&incident, &mut img);
-    top.draw(&incident, &mut img);
-    for t in &tips {
-        t.draw(&incident, &mut img);
-    }
+    let slider_color = Rgba([10, 10, 10, 255]);
+    slider.draw_color(&incident, &mut img, &slider_color);
+    top.draw_color(&incident, &mut img, &slider_color);
+    tips.draw_color(&incident, &mut img, &slider_color);
     img.save_with_format("out2.png", ImageFormat::Png).unwrap();
 }
